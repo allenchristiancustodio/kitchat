5ee64b21bac95f8358c0395efe48b20adb70e9f7
Allen Christian Custodio <allenchristiancustodio@gmail.com>
2025-03-26 09:59:17 -0400

    Created Message Endpoints and added daisyUI

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,65 @@
+import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
+
+export const getUsersForSideBar = async (req, res) => {
+  try {
+    const loggedInUser = req.user._id;
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUser },
+    }).select(-"password");
+
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.log("Error in getUsersForSideBar controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+
+    const message = await Message.find({
+      $or: [
+        { senderId: senderId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: senderId },
+      ],
+    });
+
+    res.status(200).json(message);
+  } catch (error) {
+    console.log("Error in getmMessages controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+
+    let imageUrl = null;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const message = new Message({
+      senderId,
+      receiverId: userToChatId,
+      text,
+      imageUrl,
+    });
+
+    await message.save();
+
+    //TODO: realtime functionality goes here
+
+    res.status(200).json(message);
+  } catch (error) {
+    console.log("Error in sendMessage controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 const app = express();
@@ -10,10 +12,12 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
-app.listen(PORT, () => {
+app.listen(PORT || 5000, () => {
   console.log("server is running on PORT:" + PORT);
   connectDB();
 });
